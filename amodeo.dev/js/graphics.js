9309e5794971392265da78d27ee06b0ec850f9c9
James Amodeo <jamesamodeo597@gmail.com>
2021-07-04 08:04:36 +0100

    Tower generation position is space-filling, not sequential
    Changed generation area from frustum to surface

--- amodeo.dev/js/graphics.js
+++ amodeo.dev/js/graphics.js
@@ -11,38 +11,37 @@ Queue.prototype.enqueue = function (e) {
 };
 
 Queue.prototype.dequeue = function () {
-	   return this.elements.shift();
+	return this.elements.shift();
 };
 
 Queue.prototype.isEmpty = function () {
-	   return this.elements.length == 0;
+	return this.elements.length == 0;
 };
 
 Queue.prototype.peek = function () {
-	   return !this.isEmpty() ? this.elements[0] : undefined;
+	return !this.isEmpty() ? this.elements[0] : undefined;
 };
 
 Queue.prototype.length = function() {
-	   return this.elements.length;
+	return this.elements.length;
 }
 
-const GEN_STEP = 15;
-const TOWER_SIZE = 5;
+const TOWER_COUNT = 200;
+const POS_SCAN_COUNT = 5;
+const POS_RAY_STEP = 10;
+const TOWER_SIZE = 10;
 const SCROLL_SPEED = 100;
-const MESH_ORIGIN_X = -250;
-const MESH_ORIGIN_Y = 0;
-const MESH_ORIGIN_Z = -250;
-const HORIZON_HEIGHT = 50;
-const HORIZON_DISTANCE = 500;
+const MESH_ORIGIN = { x: -250, y: 0, z: -250 };
 const ORBIT_CONTROL = true;
 const BASE_GEOMETRY = false;
-const FIX_FRUSTUM = true;
+const ORBIT_TARGET = new THREE.Vector3();
 
-const plane =
-{
-	width: 1080,
-	height: 500
-};
+const SURFACE_NEAR_WIDTH = 500;
+const SURFACE_FAR_WIDTH = 1000;
+const SURFACE_DEPTH = 500;
+const SURFACE_ORIGIN = { x: 0 - (SURFACE_NEAR_WIDTH / 2), y: 0 }
+
+let scrollSpeed = SCROLL_SPEED;
 
 const canvas = document.querySelector('#graphics');
 const renderer = new THREE.WebGLRenderer({canvas});
@@ -50,7 +49,6 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
 camera.position.set(0, 450, 500);
-camera.lookAt(0, 0, -100);
 
 if (ORBIT_CONTROL) {
 	const controls = new OrbitControls( camera, canvas );
@@ -58,20 +56,51 @@ if (ORBIT_CONTROL) {
 	controls.enablePan = true;
 	controls.minDistance = 1.2;
 	controls.maxDistance = 1000;
+	controls.target = ORBIT_TARGET;
 	controls.update();
 }
 
-const initialFrustum = new THREE.Frustum().setFromProjectionMatrix(new THREE.Matrix4().multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse));
-const geometryPlane = new THREE.PlaneGeometry( plane.width, plane.height );
-const materialPlane = new THREE.MeshBasicMaterial( {color: 'navy', side: THREE.DoubleSide} );
-const meshPlane = new THREE.Mesh( geometryPlane, materialPlane );
-meshPlane.rotation.x = Math.PI / 2;
-meshPlane.position.y = -1;
-scene.add( meshPlane );
+let surface =
+{
+	nearLeft: SURFACE_ORIGIN,
+	nearRight: { x: SURFACE_ORIGIN.x + SURFACE_NEAR_WIDTH, y: SURFACE_ORIGIN.y },
+	farLeft: { x: SURFACE_ORIGIN.x - (SURFACE_FAR_WIDTH - SURFACE_NEAR_WIDTH) / 2, y: SURFACE_ORIGIN.y - SURFACE_DEPTH },
+	farRight: { x: SURFACE_ORIGIN.x + (SURFACE_FAR_WIDTH + SURFACE_NEAR_WIDTH) / 2, y: SURFACE_ORIGIN.y - SURFACE_DEPTH},
+
+	gradient: ( ( SURFACE_FAR_WIDTH - SURFACE_NEAR_WIDTH ) / 2 ) / SURFACE_DEPTH
+}
+
+const surfaceGeometry = new THREE.BufferGeometry();
+const surfaceVertices = new Float32Array( [
+	surface.nearRight.x, 0, surface.nearRight.y,
+	surface.farLeft.x, 0, surface.farLeft.y,
+	surface.nearLeft.x, 0, surface.nearLeft.y,
+
+	surface.farRight.x, 0, surface.farRight.y,
+	surface.farLeft.x, 0, surface.farLeft.y,
+	surface.nearRight.x, 0, surface.nearRight.y
+] );
+surfaceGeometry.setAttribute( 'position', new THREE.BufferAttribute( surfaceVertices, 3 ) );
+const surfaceMaterial = new THREE.MeshBasicMaterial( { color: 'navy' } );
+const surfaceMesh = new THREE.Mesh( surfaceGeometry, surfaceMaterial );
+scene.add( surfaceMesh );
+
+const cullPlaneGeometry = new THREE.BufferGeometry();
+const cullPlaneVertices = new Float32Array( [
+	surface.nearRight.x, 0, surface.nearRight.y,
+	surface.farRight.x, 20, surface.farRight.y,
+	surface.farRight.x, 0, surface.farRight.y,
+
+	surface.nearRight.x, 20, surface.nearRight.y,
+	surface.farRight.x, 20, surface.farRight.y,
+	surface.nearRight.x, 0, surface.nearRight.y
+] );
+cullPlaneGeometry.setAttribute( 'position', new THREE.BufferAttribute( cullPlaneVertices, 3 ) );
+const cullPlaneMaterial = new THREE.MeshBasicMaterial( { color: 'grey' } );
+const cullPlaneMesh = new THREE.Mesh( cullPlaneGeometry, cullPlaneMaterial );
+scene.add( cullPlaneMesh );
 
 const towers = [];
-const freshTowers = new Queue();
-const expiredTowers = new Queue();
 
 initTowers();
 
@@ -85,15 +114,18 @@ if (BASE_GEOMETRY) {
 	mesh = new THREE.Mesh(towerGeometries, towerMaterial);
 }
 const towerMesh = mesh;
-towerMesh.position.x = MESH_ORIGIN_X;
-towerMesh.position.y = MESH_ORIGIN_Y;
-towerMesh.position.z = MESH_ORIGIN_Z;
+towerMesh.position.x = MESH_ORIGIN.x;
+towerMesh.position.y = MESH_ORIGIN.y;
+towerMesh.position.z = MESH_ORIGIN.z;
 scene.add(towerMesh);
+
+const debugLines = [];
+const debugMeshes = [];
 
 let then = 0;
 let delta = 0;
 
-document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+document.addEventListener( 'keydown', onDocumentKeyDown, false );
 
 render();
 
@@ -102,53 +134,38 @@ function Tower(pos, base, height, geometry) {
 	this.base = base;
 	this.height = height;
 	this.geometry = geometry;
-	this.status = 0;  // fresh
 }
 
 function initTowers() {
-	
-	let x, y;
-	x = GEN_STEP;
-	
-	while (x < plane.width) {
-		y = GEN_STEP;
-		while (y < plane.height) {
-			
-			const tower = genTower({x: x + rand(-1.5, 1.5), y: y + rand(-1.5, 1.5)});
-			tower.status = 1;  // initial towers are visible
-			
-			towers.push(tower);
-
-			// make leftmost column of towers fresh
-			if (x === GEN_STEP) {
-				tower.status = 0;
-				freshTowers.enqueue(towers.length - 1);
-			}
-			
-			y += GEN_STEP;
-		}
-		x += GEN_STEP;
+	for (let i = 0; i < TOWER_COUNT; i++) {
+		//const newPos = genTowerPos();
+		const tower = genTower({x: rand(-200, 500), y: rand(-200, 200)});
+		towers.push(tower);
 	}
 }
 
 function resetMeshOrigin() {
 
-	let offsetX = towerMesh.position.x - MESH_ORIGIN_X;
-	let offsetY = towerMesh.position.y - MESH_ORIGIN_Y;
-	let offsetZ = towerMesh.position.z - MESH_ORIGIN_Z;
+	let offsetX = towerMesh.position.x - MESH_ORIGIN.x;
+	let offsetY = towerMesh.position.y - MESH_ORIGIN.y;
+	let offsetZ = towerMesh.position.z - MESH_ORIGIN.z;
 
 	for (let tower of towers) {
 		translateGeometry(tower.geometry, offsetX, offsetY, offsetZ);
 		tower.pos.x += offsetX;
-		tower.pos.y += offsetY;
-		tower.pos.z += offsetZ;
+		tower.pos.y += offsetZ;
+
+		for (let p in tower.base) {
+			tower.base[p].x += offsetX;
+			tower.base[p].y += offsetZ;
+		}
 	}
 
 	translateGeometry(towerGeometries, offsetX, offsetY, offsetZ);
 	
-	towerMesh.position.x = MESH_ORIGIN_X;
-	towerMesh.position.y = MESH_ORIGIN_Y;
-	towerMesh.position.z = MESH_ORIGIN_Z;
+	towerMesh.position.x = MESH_ORIGIN.x;
+	towerMesh.position.y = MESH_ORIGIN.y;
+	towerMesh.position.z = MESH_ORIGIN.z;
 
 }
 
@@ -164,43 +181,11 @@ function translateGeometry(geometry, x, y, z) {
 }
 
 function updateTowers() {
-
 	for (let i = 0; i < towers.length; i++) {
-		if (towers[i].status == 1) {  // if visible
-			if (!geometryInFrustum(towers[i].geometry)) {
-				expiredTowers.enqueue(i);
-				towers[i].status = 2;  // expired
-			}
-		}
-	}
-
-	for (let i = 0; i < freshTowers.length(); i++) {
-		if (geometryInFrustum(towers[freshTowers.elements[i]].geometry)) {
-			if (!expiredTowers.isEmpty()) {
-				let replacedIdx = expiredTowers.dequeue();
-				
-				// generate new tower
-				let newPos = findAdjacentPos(towers[freshTowers.elements[i]]);
-				const newTower = genTower(newPos);
-
-				// replace tower in towers array
-				towers[replacedIdx] = newTower;
-				
-				// replace tower in merged geometry
-				for (let j = 0; j < 24; j++) {
-					towerGeometries.attributes.position.array[(replacedIdx * 24) + j] = newTower.geometry.attributes.position.array[j];
-
-				}
-				towerGeometries.attributes.position.needsUpdate = true;
-				
-				// fresh tower now visible
-				towers[freshTowers.elements[i]].status = 1;  // visible
-				freshTowers.elements.splice(i, 1);
-
-				// new tower is fresh
-				freshTowers.enqueue(replacedIdx);
-
-			}
+		if (!towerOnSurface(towers[i])) {
+			const newPos = genTowerPos();
+			const newTower = genTower(newPos);
+			updateTowerGeometry(i, newTower);			
 		}
 	}
 
@@ -209,56 +194,155 @@ function updateTowers() {
 	}
 }
 
-function posInTower(pos) {
+/*
+function genTowerPos() {
+	const scanOrigin = new THREE.Vector3(surface.nearLeft.x, 1, surface.nearLeft.y);
+	const scanEnd = new THREE.Vector3(surface.farLeft.x, 1, surface.farLeft.y);
+	const scanStep = new THREE.Vector3().subVectors(scanEnd, scanOrigin).divideScalar(POS_SCAN_COUNT + 1);	
 
-	const raycaster = new THREE.Raycaster();
-	const origin = new THREE.Vector3(pos.x, 0, pos.y).applyMatrix4( baseMesh.matrixWorld );
-	const direction = new THREE.Vector3(100,0,100);
+	let scanPos;
+	let furthestDistance = 0;
+	let furthestIntersection = null;
 
-	raycaster.set(origin, direction)
-	raycaster.params.Mesh.threshold = 3;
-	const intersects = raycaster.intersectObjects( baseMesh );
+	while (debugLines.length > 0) {
+		debugLines.pop().removeFromParent();
+	}
 
-    direction.normalize();
+	for (let i = 0; i < POS_SCAN_COUNT; i++) {
+		
+		//scanPos = scanOrigin.clone().add(scanStep.clone().multiplyScalar((i + 1) + rand(-POS_SCAN_NOISE, POS_SCAN_NOISE)));
+		scanPos = scanOrigin.clone().add(scanStep.clone().multiplyScalar((Math.floor(rand(0, POS_SCAN_COUNT-1)) + 1) + rand(-POS_SCAN_NOISE, POS_SCAN_NOISE)));
 
-    var distance = 100; // at what distance to determine pointB
+		const raycaster = new THREE.Raycaster();
+		const direction = new THREE.Vector3(1,0,0);
 
-    var end = new THREE.Vector3();
-    end.addVectors ( origin, direction.multiplyScalar( distance ) );
+		raycaster.set(scanPos, direction)
+		const intersections = raycaster.intersectObjects( [towerMesh, cullPlaneMesh] );		
 
-    const points = [];
-    points.push( origin );
-    points.push( end );
-	const geometry = new THREE.BufferGeometry().setFromPoints(points);
-    var material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
-    var line = new THREE.Line( geometry, material );
-    scene.add( line );
+		const intersect = intersections[0];
+
+		if (intersections.length == 1) {
+			furthestDistance = Number.POSITIVE_INFINITY;
+			furthestIntersection = intersect.point;
+		} else if (intersect.distance > furthestDistance) {
+			furthestDistance = intersect.distance;
+			furthestIntersection = intersect.point;
+		}
+		
+		const distance = 50;
+		let end = intersections.length > 1 ? intersections[0].point : new THREE.Vector3().addVectors( scanPos,  direction.multiplyScalar( distance ) );
+		const points = [];
+		points.push( scanPos );
+		points.push( end );
+		const geometry = new THREE.BufferGeometry().setFromPoints(points);
+		const material = new THREE.LineBasicMaterial( { color : intersections.length == 0 ? 0x00ff00 : 0xff0000 } );
+		const line = new THREE.Line( geometry, material );
+		debugLines.push( line );
+		scene.add( line );
+	}
+
+	let pos = { x: furthestIntersection.x - towerMesh.position.x, y: furthestIntersection.z - towerMesh.position.z };
+	let xOffset = TOWER_SIZE + rand(3, 20);
+	pos.x -= xOffset;
+
+	return pos;
+}
+*/
+
+function pointBuildable(point) {
+
+	let pos = {x: point.x - towerMesh.position.x, y: point.y - towerMesh.position.z};
+	const basePoints = [
+		{ x: pos.x, y: pos.y },
+		{ x: pos.x, y: pos.y + TOWER_SIZE }, 
+		{ x: pos.x + TOWER_SIZE, y: pos.y + TOWER_SIZE }, 
+		{ x: pos.x + TOWER_SIZE, y: pos.y }
+	];
 	
-	return intersects.length > 0;
+	for (const tower of towers) {
+		const shape = {
+			topLeft: tower.base.p1,
+			topRight: tower.base.p4,
+			botLeft: tower.base.p2,
+			botRight: tower.base.p3
+		};
+		for (const point of basePoints) {
+			if (pointInShape(point, shape)) {
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
-function findAdjacentPos(tower) {
-	let newPos = tower.pos;
-	/*if (posInTower(newPos)) {
-		console.log("BAM");
-	}*/
-	newPos.x -= GEN_STEP;
-	newPos.x += rand(-1.5, 1.5);
-	newPos.y += rand(-1.5, 1.5);
-	return newPos;
+function genTowerPos() {
+
+	const scanOrigin = new THREE.Vector3(surface.nearRight.x-1, 1, surface.nearRight.y+1);
+	const scanEnd = new THREE.Vector3(surface.farRight.x-1, 1, surface.farRight.y-1);
+	const scanInterval = new THREE.Vector3().subVectors(scanEnd, scanOrigin).divideScalar(POS_SCAN_COUNT);
+	const rayDir = { x: -1, y: 0 };
+
+	const surfaceShape = {
+		topLeft: surface.farLeft,
+		topRight: surface.farRight,
+		botLeft: surface.nearLeft,
+		botRight: surface.nearRight
+	};
+
+	let emptyPos = { x: null, y: null };
+	//let emptyPosDist = Number.POSITIVE_INFINITY;
+
+	for (let i = 0; i < POS_SCAN_COUNT; i++) {
+		
+		const rayOrigin = scanOrigin.clone().add(scanInterval.clone().multiplyScalar(i + rand(0, 1)));
+		
+		let rayPos = { x: rayOrigin.x, y: rayOrigin.z };
+		//let travelledDist = 0;
+		let rayEnded = false;
+
+		//while (travelledDist < emptyPosDist && !rayEnded && pointInShape(rayPos, surfaceShape)) {
+		while (!rayEnded && pointInShape(rayPos, surfaceShape)) {
+		
+			if (pointBuildable(rayPos)) {
+				emptyPos.x = rayPos.x;
+				emptyPos.y = rayPos.y;
+				//emptyPosDist = travelledDist;
+				rayEnded = true;
+			}
+
+			rayPos.x += rayDir.x * POS_RAY_STEP;
+			rayPos.y += rayDir.y * POS_RAY_STEP;
+			//travelledDist += POS_RAY_STEP;
+		}
+	}
+
+	let pos = {x: emptyPos.x - towerMesh.position.x, y: emptyPos.y - towerMesh.position.z};
+	return pos;
 }
 
 function genTower({x, y}) {
 	
 	let tower = new Tower({x, y}, null, Math.floor(rand(5, 50)), null);
 	tower.base = {
-		p1: { x: x - TOWER_SIZE, y: y - TOWER_SIZE },
-		p2: { x: x - TOWER_SIZE, y: y }, 
-		p3: { x: x, y: y }, 
-		p4: { x: x, y: y - TOWER_SIZE }}
+		p1: { x: x, y: y },
+		p2: { x: x, y: y + TOWER_SIZE }, 
+		p3: { x: x + TOWER_SIZE, y: y + TOWER_SIZE }, 
+		p4: { x: x + TOWER_SIZE, y: y }}
 	const color = new THREE.Color(`hsl(${THREE.MathUtils.mapLinear(tower.height, 5, 50, 127, 0)}, 30%, 50%)`);
 	tower.geometry = BASE_GEOMETRY ? genBaseGeometry(tower.base, color) : genTowerGeometry(tower.base, tower.height, color);
 	return tower;
+}
+
+function updateTowerGeometry(idx, tower) {
+
+	towers[idx] = tower;
+
+	for (let i = 0; i < 24; i++) {
+		towerGeometries.attributes.position.array[(idx * 24) + i] = tower.geometry.attributes.position.array[i];
+	}
+
+	towerGeometries.attributes.position.needsUpdate = true;
+
 }
 
 function genBaseGeometry({p1, p2, p3, p4}, color) {
@@ -302,8 +386,6 @@ function genTowerGeometry({p1, p2, p3, p4}, height, color) {
 	vertices.push(p4.x, height, p4.y);
 
 	const indices = [];
-	indices.push(2, 1, 0);
-	indices.push(0, 3, 2);
 	indices.push(0, 1, 4);
 	indices.push(4, 1, 5);
 	indices.push(1, 2, 5);
@@ -339,22 +421,43 @@ function setTowerColor(idx, color) {
 	towerGeometries.attributes.color.needsUpdate = true;
 }
 
-function geometryInFrustum(geometry) {
-	camera.updateMatrix();
-	camera.updateMatrixWorld();
-	
-	var frustum;
-	if (FIX_FRUSTUM) {
-		frustum = initialFrustum;
-	} else {
-		frustum = new THREE.Frustum();
-		frustum.setFromProjectionMatrix(new THREE.Matrix4().multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse));
+function pointInShape(point, {topLeft, topRight, botLeft, botRight}) {
+
+	const gradTLTR = (topRight.y - topLeft.y) / (topRight.x - topLeft.x);
+	const gradBLTL = -(topLeft.x - botLeft.x) / (topLeft.y - botLeft.y);
+	const gradBLBR = (botRight.y -botLeft.y) / (botRight.x - botLeft.x);
+	const gradBRTR = -(topRight.x - botRight.x) / (topRight.y - botRight.y);
+
+	return (point.x >= botLeft.x + gradBLTL * (botLeft.y - point.y) && 
+		point.x <= botRight.x + gradBRTR * (botRight.y - point.y) &&
+		point.y <= botLeft.y + gradBLBR * (point.x - botLeft.x) &&
+		point.y >= topLeft.y + gradTLTR * (point.x - topLeft.x));
+
+}
+
+function towerOnSurface(tower) {
+
+	for (let corner in tower.base) {
+
+		const point = {
+			x: tower.base[corner].x + towerMesh.position.x, 
+			y: tower.base[corner].y + towerMesh.position.z
+		};
+
+		const shape = {
+			topLeft: surface.farLeft,
+			topRight: surface.farRight,
+			botLeft: surface.nearLeft,
+			botRight: surface.nearRight
+		};
+
+		if (pointInShape(point, shape)) {
+			return true;
+		}
+
 	}
 
-	const box = new THREE.Box3();
-	box.copy( geometry.boundingBox ).applyMatrix4( towerMesh.matrixWorld );
-	return frustum.intersectsBox(box);
-	
+	return false;
 }
 	
 function rand(min, max) {
@@ -388,10 +491,12 @@ function update(time) {
 	then = time;
 	
 	if (delta) {
-		towerMesh.position.x += (delta * SCROLL_SPEED);
+		towerMesh.position.x += (delta * scrollSpeed);
 	}
 	
-	updateTowers();
+	if (time > 1) {
+		updateTowers();
+	}
 }
 
 function render(time) {
@@ -410,6 +515,8 @@ function render(time) {
 	
 }
 
-function onDocumentMouseDown( event ) {
+function onDocumentKeyDown( event ) {
 	event.preventDefault();
+	updateTowers();
+	scrollSpeed == 0 ? scrollSpeed = SCROLL_SPEED : scrollSpeed = 0;
 }
